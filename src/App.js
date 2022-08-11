dda2e0419dff39a7a787c0de9ced7e1671d283b4
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-08-11 12:04:39 +0100

    code clean and navbar started

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <div className="nav-container"></div>
+        <div className="nav-container">
+          <div className="logo">
+            <h1>IPS Logistics</h1>
+            <i class="fa-solid fa-truck-fast"></i>
+          </div>
+
+          <div className="liists">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Track</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Our Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Branches</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="acc">
+            <button>Login</button>
+            <button>Sign UP</button>
+          </div>
+        </div>
       </nav>
     </div>
   );
